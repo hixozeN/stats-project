3f0317cbe3d43edd07a27df9266c44444615d9cb
Vladislav K <aljkeee@gmail.com>
2023-05-16 03:01:27 +0300

    Настраивает gitignore

---
+++ config/build/buildDevServer.ts
@@ -0,0 +1,9 @@
+import { BuildOptions } from "./types/config";
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+
+export default function buildDevServer(options: BuildOptions): DevServerConfiguration {
+  return {
+    port: options.port,
+    open: true,
+  };
+};
