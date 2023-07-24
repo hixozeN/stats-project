8813099c4c8ad9c63aff86a88ef99a3abb2ced12
Vladislav K <aljkeee@gmail.com>
2023-07-24 04:55:57 +0300

    Добавляет jest, покрывает тестами кастомный хук, настраивает тестовую среду

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,32 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+  test('With only first param', () => {
+    expect(classNames('test'))
+      .toBe('test');
+  });
+
+  test('With additional class param', () => {
+    const expected = 'test additional';
+    expect(classNames('test', {}, ['additional']))
+      .toBe(expected);
+  });
+
+  test('With all params', () => {
+    const expected = 'test additional hidden';
+    expect(classNames('test', { hidden: true }, ['additional']))
+      .toBe(expected);
+  });
+
+  test('With false mod', () => {
+    const expected = 'test additional hidden';
+    expect(classNames('test', { visible: false, hidden: true }, ['additional']))
+      .toBe(expected);
+  });
+
+  test('With undefined mod', () => {
+    const expected = 'test additional hidden';
+    expect(classNames('test', { visible: false, hidden: true, hovered: undefined }, ['additional']))
+      .toBe(expected);
+  });
+});
