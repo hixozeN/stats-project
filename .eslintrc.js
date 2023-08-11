4f98f5a4ceeb40643a37c636e7412881f2c72b8f
Vladislav K <aljkeee@gmail.com>
2023-08-11 12:05:44 +0300

    Добавляет правило линтера

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,7 +44,7 @@ module.exports = {
     'no-underscore-dangle': 'off',
     'i18next/no-literal-string': [
       'error',
-      { markupOnly: true, ignoreAttribute: ['data-testid'] },
+      { markupOnly: true, ignoreAttribute: ['data-testid', 'to'] },
     ],
     'max-len': ['error', { ignoreComments: true, code: 100 }],
   },
