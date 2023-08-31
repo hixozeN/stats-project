f8531edc0d0fe16b2c189b6be0d3f224e165882c
Vladislav K <aljkeee@gmail.com>
2023-08-31 18:51:50 +0300

    Добавляет story для ThemeSwitcher

---
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Theme } from 'app/providers/ThemeProvider';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { ThemeSwitcher } from './ThemeSwitcher';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof ThemeSwitcher> = {
+  title: 'widgets/ThemeSwitcher',
+  component: ThemeSwitcher,
+  parameters: {
+
+  },
+  args: {},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ThemeSwitcher>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Light: Story = {
+  args: {},
+};
+
+export const Dark: Story = {
+  args: {},
+  decorators: [ThemeDecorator(Theme.DARK)],
+};
