a8e5a28459d1681c42e972befa2a3a67e51db608
Vladislav K <aljkeee@gmail.com>
2023-08-31 18:35:09 +0300

    Добавляет story для AppLink

---
+++ src/shared/ui/AppLink/AppLink.stories.tsx
@@ -0,0 +1,56 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Theme } from 'app/providers/ThemeProvider';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { AppLink, AppLinkTheme } from './AppLink';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof AppLink> = {
+  title: 'shared/AppLink',
+  component: AppLink,
+  parameters: {
+
+  },
+  args: {
+    to: '#',
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof AppLink>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+  args: {
+    theme: AppLinkTheme.PRIMARY,
+    children: 'Link',
+  },
+};
+
+export const PrimaryDark: Story = {
+  args: {
+    theme: AppLinkTheme.PRIMARY,
+    children: 'Link',
+  },
+  decorators: [ThemeDecorator(Theme.DARK)],
+};
+
+export const Secondary: Story = {
+  args: {
+    theme: AppLinkTheme.SECONDARY,
+    children: 'Link',
+  },
+};
+
+export const SecondaryDark: Story = {
+  args: {
+    theme: AppLinkTheme.SECONDARY,
+    children: 'Link',
+  },
+  decorators: [ThemeDecorator(Theme.DARK)],
+};
+
+// export const Dark: Story = {
+//   args: {},
+//   decorators: [ThemeDecorator(Theme.DARK)],
+// };
