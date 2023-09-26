1c75759c1cd6be3b3a60cbe58be69c0b8f5b4f1b
Vladislav K <aljkeee@gmail.com>
2023-09-26 16:00:46 +0300

    Добавляет обертку рендера компонентов для тестов

---
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from 'react';
+import { render } from '@testing-library/react';
+import { I18nextProvider } from 'react-i18next';
+import i18nForTests from 'shared/config/i18n/i18nForTests';
+import { MemoryRouter } from 'react-router-dom';
+
+export interface ComponentRenderOptions {
+  route?: string;
+}
+
+export function componentRender(component: ReactNode, options: ComponentRenderOptions = {}) {
+  const { route = '/' } = options;
+
+  return render(
+    <MemoryRouter initialEntries={[route]}>
+      <I18nextProvider i18n={i18nForTests}>
+        {component}
+      </I18nextProvider>
+    </MemoryRouter>,
+  );
+}
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,20 +1,21 @@
 import { fireEvent, screen } from '@testing-library/react';
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
-import {
-  renderWithTranslation,
-} from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 
 describe('Sidebar', () => {
   test('Тест рендера сайдбара', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
 
   test('Тест тоггла сайдбара', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     const toggleBtn = screen.getByTestId('sidebar-toggle');
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
-    fireEvent.click(toggleBtn);
+    // есть класс свернутого сайдбара
     expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
+    fireEvent.click(toggleBtn);
+    // нет класса свернутого сайдбара после клика по кнопке свернуть
+    expect(screen.getByTestId('sidebar')).not.toHaveClass('collapsed');
   });
 });
