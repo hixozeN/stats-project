85c8b75be8c33ef18a1233f07196f85dd6312ba4
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2023-11-10 18:49:41 +0300

    создает секции промо и как играть

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,20 +1,41 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { ErrorBoundary } from 'app/providers/ErrorBoundary';
-import { TestErrorBoundary } from 'features/TestErrorBoundary/ui/TestErrorBoundary';
+import cls from './MainPage.module.scss';
 
 function MainPage() {
   const { t } = useTranslation('main');
 
   return (
-    <div>
-      <h1>
-        {t('Главная страница')}
-        <ErrorBoundary>
-          <TestErrorBoundary />
-        </ErrorBoundary>
-      </h1>
-    </div>
+    <>
+      <section className={cls.promo}>
+        <div className={cls.background}>
+          <h1 className={cls.title}>Соревновательные матчи и турниры</h1>
+          <p className={cls.text}>Задача организации, в особенности же новая модель организационной деятельности позволяет оценить значение позиций, занимаемых участниками в отношении поставленных задач. </p>
+        </div>
+      </section>
+      <section className={cls.manual}>
+        <h2 className={cls.title}>Как играть</h2>
+        <ol className={cls.list}>
+          <li className={cls.item}>
+            <div className={cls.shadow} />
+            <p className={cls.text}>Регистрируй аккаунт</p>
+          </li>
+          <li className={cls.item}>
+            <div className={cls.shadow} />
+            <p>Создавай или вступай в команду</p>
+          </li>
+          <li className={cls.item}>
+            <div className={cls.shadow} />
+            <p>Регистрируйся в турнире</p>
+          </li>
+          <li className={cls.item}>
+            <div className={cls.shadow} />
+            <p>Играй и побеждай</p>
+          </li>
+        </ol>
+      </section>
+    </>
+
   );
 }
 
