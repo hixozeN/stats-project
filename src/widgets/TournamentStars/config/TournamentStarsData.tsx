bf1f65e0587d7e84a46f61875fdfe0b0e45d746d
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2023-11-29 18:36:07 +0300

    добавляет структуру данных для звезд турнира

---
+++ src/widgets/TournamentStars/config/TournamentStarsData.tsx
@@ -0,0 +1,59 @@
+import icon1 from 'shared/assets/images/lending-test/icon-lending-test.jpg';
+import img1 from 'shared/assets/images/lending-test/image 30.png';
+import img2 from 'shared/assets/images/lending-test/image 31.png';
+import img3 from 'shared/assets/images/lending-test/image 32.png';
+import img4 from 'shared/assets/images/lending-test/image 33.png';
+import icon2 from 'shared/assets/images/lending-test/icon-lenging-test-jpg.jpg';
+
+export const dataList = [
+  {
+    id: 1,
+    icon: icon1,
+    team: 'OLRCY',
+    rating: 2674,
+    tournaments: 27,
+    modes: 25,
+    participants: 9,
+    awards: [img1, img2, img3, img4],
+  },
+  {
+    id: 2,
+    icon: icon2,
+    team: 'No Mercy',
+    rating: 2674,
+    tournaments: 12,
+    modes: 25,
+    participants: 1,
+    awards: [img4],
+  },
+  {
+    id: 3,
+    icon: icon1,
+    team: 'Ace Ventura',
+    rating: 2674,
+    tournaments: 43,
+    modes: 25,
+    participants: 9,
+    awards: [img2],
+  },
+  {
+    id: 4,
+    icon: icon1,
+    team: '7STAR',
+    rating: 2674,
+    tournaments: 0,
+    modes: 25,
+    participants: 2,
+    awards: [img1, img2],
+  },
+  {
+    id: 5,
+    icon: icon1,
+    team: 'GuchiGangTeam',
+    rating: 1245,
+    tournaments: 0,
+    modes: 25,
+    participants: 4,
+    awards: [img3],
+  },
+];
