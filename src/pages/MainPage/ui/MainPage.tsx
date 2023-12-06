fab1085674290b6d81581663cab9b73f128fd5fa
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2023-12-06 21:35:05 +0300

    объядиняет две секции в одну

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -11,28 +11,22 @@ function MainPage() {
       <section className={cls.promo}>
         <div className={cls.background}>
           <h1 className={cls.title}>{t('Соревновательные матчи и турниры')}</h1>
+          <h2 className={cls.subtitle}>{t('Как играть')}</h2>
+          <ol className={cls.list}>
+            <li className={cls.item}>
+              <p className={cls.text}>{t('Регистрируй аккаунт')}</p>
+            </li>
+            <li className={cls.item}>
+              <p className={cls.text}>{t('Создавай или вступай в команду')}</p>
+            </li>
+            <li className={cls.item}>
+              <p className={cls.text}>{t('Регистрируйся в турнире')}</p>
+            </li>
+            <li className={cls.item}>
+              <p className={cls.text}>{t('Играй и побеждай')}</p>
+            </li>
+          </ol>
         </div>
-      </section>
-      <section className={cls.manual}>
-        <h2 className={cls.title}>{t('Как играть')}</h2>
-        <ol className={cls.list}>
-          <li className={cls.item}>
-            <div className={cls.shadow} />
-            <p className={cls.text}>{t('Регистрируй аккаунт')}</p>
-          </li>
-          <li className={cls.item}>
-            <div className={cls.shadow} />
-            <p className={cls.text}>{t('Создавай или вступай в команду')}</p>
-          </li>
-          <li className={cls.item}>
-            <div className={cls.shadow} />
-            <p className={cls.text}>{t('Регистрируйся в турнире')}</p>
-          </li>
-          <li className={cls.item}>
-            <div className={cls.shadow} />
-            <p className={cls.text}>{t('Играй и побеждай')}</p>
-          </li>
-        </ol>
       </section>
       <TournamentStars />
     </>
