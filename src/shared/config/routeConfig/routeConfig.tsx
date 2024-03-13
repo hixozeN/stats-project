7dc90056fb05e3570ba111cfa7e5c995d3939052
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2024-03-13 16:06:54 +0300

    добавляет ссылку на страницу клана

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -12,8 +12,8 @@ import AppLayout from 'app/layouts/ui/AppLayout/AppLayout';
 import { UserProfileForm } from 'entities/User';
 import ReduxLayout from 'app/layouts/ReduxLayout/ReduxLayout';
 import { TournamentsPage } from 'pages/TournamentsPage/index';
-import { TeamPage } from 'shared/ui/Button/Button.stories';
 import TeamsPage from 'pages/TeamsPage/ui/TeamsPage';
+import { TeamPage } from 'pages/TeamPage';
 import { UserPage } from 'pages/UserPage';
 import { AuthorizationLestaPage } from 'pages/AuthorizationLestaPage/AuthorizationLestaPage';
 
@@ -23,6 +23,7 @@ export enum AppRoutes {
   MATCHES = 'matches',
   TOURNAMENTS = 'tournaments',
   TEAMS = 'teams',
+  TEAM = 'team',
   FRIENDS = 'friends',
   AUTH = 'auth',
   AUTH_LESTA = 'authLesta',
@@ -43,6 +44,7 @@ export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.MATCHES]: '/matches',
   [AppRoutes.TOURNAMENTS]: '/tournaments',
   [AppRoutes.TEAMS]: '/teams',
+  [AppRoutes.TEAM]: '/team/:clanId',
   [AppRoutes.FRIENDS]: '/friends',
   [AppRoutes.AUTH]: '/auth',
   [AppRoutes.AUTH_LESTA]: '/auth/lesta',
@@ -85,6 +87,10 @@ export const routerConfiguration = createBrowserRouter([
           {
             path: RoutePath.teams,
             element: <TeamsPage />,
+          },
+          {
+            path: RoutePath.team,
+            element: <TeamPage />,
           },
           {
             path: RoutePath.friends,
