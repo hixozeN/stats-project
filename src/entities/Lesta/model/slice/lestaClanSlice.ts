cd1513b6ee2376b269bc72c42304b2fa643b3586
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2024-03-13 20:08:11 +0300

    создает функцию для сбора ID пользователей

---
+++ src/entities/Lesta/model/slice/lestaClanSlice.ts
@@ -0,0 +1,43 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { LestaClanSchema } from '../types';
+import { LestaClan } from '../types/clans';
+import { fetchLestaClanData } from '../services/fetchLestaClanData/fetchLestaClanData';
+
+const initialState: LestaClanSchema = {
+  isLoading: false,
+  clan: null,
+  error: null,
+  isNotFound: false,
+};
+
+export const lestaClanSlice = createSlice({
+  name: 'LESTA_CLAN_DATA',
+  initialState,
+  reducers: {
+    setClanData: (state, action: PayloadAction<LestaClan>) => {
+      state.clan = action.payload;
+    },
+    setNotFound: (state, action: PayloadAction<boolean>) => {
+      state.isNotFound = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchLestaClanData.pending, (state) => {
+        state.error = '';
+        state.isLoading = true;
+        state.isNotFound = false;
+      })
+      .addCase(fetchLestaClanData.rejected, (state, { payload }) => {
+        state.error = payload;
+        state.isLoading = false;
+      })
+      .addCase(fetchLestaClanData.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+
+        if (!payload.clan_id) state.isNotFound = true;
+      });
+  },
+});
+
+export const { actions: clanActions, reducer: clanReducers } = lestaClanSlice;
