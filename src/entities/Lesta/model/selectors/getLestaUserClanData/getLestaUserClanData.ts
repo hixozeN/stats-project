86cf35f1b98cdc07477356e034cba8d5f6fe3c5c
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2024-03-13 16:26:49 +0300

    обновляет стейт с лесты на леста клан

--- src/entities/Lesta/model/selectors/getLestaUserClanData/getLestaUserClanData.ts
+++ src/entities/Lesta/model/selectors/getLestaUserClanData/getLestaUserClanData.ts
@@ -1,3 +1,3 @@
 import { StateSchema } from 'app/providers/StoreProvider/index';
 
-export const getLestaUserClanData = (state: StateSchema) => state?.lesta?.user?.clan || null;
+export const getLestaUserClanData = (state: StateSchema) => state?.lestaClanData?.clan || null;
