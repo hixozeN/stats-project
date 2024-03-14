c18f59a5aa57b0ffbddc444fda256133917b6d13
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2024-03-14 22:18:11 +0300

    добавляет данные клана в схему

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -13,13 +13,14 @@ import {
 import { AxiosInstance } from 'axios';
 import { NavigateOptions, To } from 'react-router-dom';
 import { TournamentSchema } from 'entities/Tournament/model/types/tournament';
-import { LestaSchema } from 'entities/Lesta/index';
+import { LestaClanSchema, LestaSchema } from 'entities/Lesta/index';
 
 export interface StateSchema {
   user: UserSchema;
   searchForm: SearchSchema;
   tournaments: TournamentSchema;
   lesta: LestaSchema,
+  lestaClanData: LestaClanSchema,
   // async reducers
   authForm?: AuthSchema;
 }
