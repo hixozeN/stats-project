4b5898b6e5a5d0b275736498dae97719862acfa9
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2024-03-19 19:53:51 +0300

    меняет проверку длины массива

--- src/pages/TeamPage/lib/generateUserIdList.ts
+++ src/pages/TeamPage/lib/generateUserIdList.ts
@@ -2,7 +2,7 @@ import { LestaClanMember } from 'entities/Lesta/model/types/clans';
 
 export const generateUserIdList = (arr: LestaClanMember[]): number[] => {
   const result: number[] = [];
-  if (arr.length === 0) return [];
+  if (!arr.length) return [];
 
   arr.forEach((item) => result.push(item.account_id));
   return result;
