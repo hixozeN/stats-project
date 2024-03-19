d204d11c414eeb50cb78c1624ca85d028fe283ca
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2024-03-19 20:28:16 +0300

    добавляет селекторы и убирает пропсы

--- src/widgets/TeamContent/ui/TeamAvatar/ui/TeamAvatar.tsx
+++ src/widgets/TeamContent/ui/TeamAvatar/ui/TeamAvatar.tsx
@@ -1,20 +1,27 @@
+import { useSelector } from 'react-redux';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { getLestaClanLogo, getLestaClanName } from 'entities/Lesta';
 import cls from './TeamAvatar.module.scss';
 
 interface TeamAvatarProps {
   className?: string;
-  logo: number;
-  name: string;
 }
 
 export const TeamAvatar = (props: TeamAvatarProps) => {
-  const { className, logo, name } = props;
+  const { className } = props;
+  const logo = useSelector(getLestaClanLogo);
+  const name = useSelector(getLestaClanName);
+
+  const renderLogo = (): string => {
+    if (!logo) return '';
+    return `https://wotblitz-gc.gcdn.co/icons/clanEmblems1x/clan-icon-v2-${logo}.png`;
+  };
 
   return (
     <section className={classNames(cls.TeamAvatar, {}, [className])}>
       <img
         className={cls.logo}
-        src={`https://wotblitz-gc.gcdn.co/icons/clanEmblems1x/clan-icon-v2-${logo}.png`}
+        src={renderLogo()}
         alt={name}
       />
     </section>
