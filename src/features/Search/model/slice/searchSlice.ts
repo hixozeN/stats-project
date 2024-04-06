5ef4f11fd49b67d020d93edbfc6ea588f118e546
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2024-04-06 16:59:44 +0300

    добавляет доп стейты

--- src/features/Search/model/slice/searchSlice.ts
+++ src/features/Search/model/slice/searchSlice.ts
@@ -1,15 +1,24 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { searchUsersAndClans } from 'features/Search';
 import { FoundClanData, FoundPlayerData } from '../types/FoundData';
 
 export interface SearchSchema {
   search: string;
   players: FoundPlayerData[],
   clans: FoundClanData[],
+  isLoading: boolean,
+  isNotFoundClans: boolean,
+  isNotFoundPlayers: boolean,
+  error: string,
 }
 const initialState: SearchSchema = {
   search: '',
   players: [],
   clans: [],
+  isLoading: false,
+  isNotFoundClans: false,
+  isNotFoundPlayers: false,
+  error: '',
 };
 export const searchSlice = createSlice({
   name: 'searchForm',
@@ -24,6 +33,24 @@ export const searchSlice = createSlice({
     setFoundClans: (state, action: PayloadAction<FoundClanData[]>) => {
       state.clans = action.payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(searchUsersAndClans.pending, (state) => {
+        state.isLoading = true;
+        state.isNotFoundClans = false;
+        state.isNotFoundPlayers = false;
+      })
+      .addCase(searchUsersAndClans.rejected, (state, { payload }) => {
+        state.error = payload;
+        state.isLoading = false;
+      })
+      .addCase(searchUsersAndClans.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+
+        if (!payload.clans.length) state.isNotFoundClans = true;
+        if (!payload.players.length) state.isNotFoundPlayers = true;
+      });
   },
 });
 
