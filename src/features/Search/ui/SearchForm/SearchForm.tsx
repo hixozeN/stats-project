5338acf6ddc69f67c29f2ffc7807ac70e9521406
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2024-04-06 13:56:24 +0300

    добавляет логику поиска

--- src/features/Search/ui/SearchForm/SearchForm.tsx
+++ src/features/Search/ui/SearchForm/SearchForm.tsx
@@ -1,49 +1,102 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import { FormEvent, memo, useCallback } from 'react';
+import React, {
+  memo, useCallback, useEffect, useState,
+} from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
-import { searchUsersAndClans } from 'features/Search/index';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button } from 'shared/ui/Button/Button';
+import { useClickOutside } from 'shared/hooks/useClickOutside';
+import useDebounce from 'shared/hooks/useDebounce';
 import { searchActions } from '../../model/slice/searchSlice';
 import { getSearchState } from '../../model/selectors/getSearchState/getSearchState';
+import { searchUsersAndClans } from '../../model/services/searchUsersAndClans/searchUsersAndClans';
 import { SearchInput } from '../SearchInput/SearchInput';
 import cls from './SearchForm.module.scss';
 
 interface IAuthFormProps {
   className?: string;
+  isOpen?: boolean;
+  setIsOpen?: React.Dispatch<React.SetStateAction<boolean>>;
+  isOpenPopup?: boolean;
+  isMobile?: boolean;
+  resultsRef?: React.MutableRefObject<HTMLInputElement>;
 }
 
 export const SearchForm = memo((props: IAuthFormProps) => {
-  const { className } = props;
+  const {
+    className,
+    isOpen,
+    setIsOpen,
+    isOpenPopup,
+    isMobile,
+    resultsRef,
+  } = props;
   const { t } = useTranslation('search');
   const dispatch = useDispatch();
   const { search } = useSelector(getSearchState);
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const [searchValue, setSearchValue] = useState<string>('');
+  const debouncedSearch = useDebounce(searchValue, 500);
 
-  const onChangeSearch = useCallback(
-    (e) => {
-      const { value } = e.target;
-      dispatch(searchActions.setSearch(value));
-    },
-    [dispatch],
-  );
+  useClickOutside(resultsRef, () => {
+    if (isOpen) setTimeout(() => setIsOpen(false), 50);
+  }, isMobile);
 
-  const handleSubmit = useCallback(async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    dispatch(searchUsersAndClans({ string: search }));
-  }, [search, dispatch]);
+  const onFocus = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    if (evt.target.value.length >= 1 && !isMobile) {
+      setIsOpen(true);
+    }
+  };
+
+  const onClickClear = () => {
+    setSearchValue('');
+    dispatch(searchActions.setSearch(''));
+    inputRef.current.focus();
+    if (!isMobile) {
+      setIsOpen(false);
+    }
+  };
+
+  const handleSubmit = useCallback(async (value: string) => {
+    dispatch(searchUsersAndClans({ string: value, limit: 10 }));
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (debouncedSearch.length >= 2) {
+      handleSubmit(debouncedSearch);
+    }
+  }, [debouncedSearch, handleSubmit]);
+
+  const onChangeSearch = useCallback((evt: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = evt.target;
+    dispatch(searchActions.setSearch(value));
+
+    if (!isMobile) {
+      setIsOpen(value !== '');
+    }
+
+    setSearchValue(value);
+  }, [dispatch, setIsOpen, isMobile]);
 
   return (
-    <form
-      className={classNames(cls.SearchForm, {}, [className])}
-      onSubmit={handleSubmit}
-      autoComplete="off"
-    >
-      <SearchInput
-        id="search"
-        type="search"
-        placeholder={t('Поиск')}
-        onChange={onChangeSearch}
-        value={search}
-      />
-    </form>
+    <div className={classNames(cls.container, { [cls.opened]: isMobile })}>
+      <form
+        className={classNames(cls.SearchForm, {}, [className])}
+        autoComplete="off"
+        onSubmit={(evt) => { evt.preventDefault(); handleSubmit(search); }}
+      >
+        <SearchInput
+          ref={inputRef}
+          id="search"
+          type="search"
+          placeholder={t('Поиск')}
+          onChange={onChangeSearch}
+          value={search}
+          isOpenPopup={isOpenPopup}
+          onFocus={onFocus}
+        />
+      </form>
+      {search && !isMobile && <Button className={cls.button} theme="clear" variant="close" onClick={onClickClear} />}
+    </div>
   );
 });
