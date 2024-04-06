f106a21febd3c01905395e87b13229aad8b713ff
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2024-04-06 17:02:41 +0300

    добавляет доп селекторы поиска

---
+++ src/features/Search/model/selectors/getSearchClans/getSearchClans.ts
@@ -0,0 +1,3 @@
+import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
+
+export const getSearchClans = (state: StateSchema) => state?.searchForm?.clans || null;
---
+++ src/features/Search/model/selectors/getSearchPlayers/getSearchPlayers.ts
@@ -0,0 +1,3 @@
+import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
+
+export const getSearchPlayers = (state: StateSchema) => state?.searchForm?.players || null;
