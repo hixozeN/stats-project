017028ce1db88073737ce5dc668f083872973d58
Vladislav K. <116466726+hixozeN@users.noreply.github.com>
2024-04-08 17:21:32 +0300

    Добавляет отдельный StoreProvider для RTL

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -3,8 +3,9 @@ import { render } from '@testing-library/react';
 import { I18nextProvider } from 'react-i18next';
 import i18nForTests from 'shared/config/i18n/i18nForTests';
 import { MemoryRouter } from 'react-router-dom';
-import { StateSchema, StoreProvider } from 'app/providers/StoreProvider/index';
+import { StateSchema } from 'app/providers/StoreProvider';
 import { DeepPartial } from '@reduxjs/toolkit';
+import { JestStoreProvider } from './JestStoreProvider';
 
 export interface ComponentRenderOptions {
   route?: string;
@@ -16,13 +17,13 @@ export function componentRender(component: ReactNode, options: ComponentRenderOp
 
   return render(
     <MemoryRouter initialEntries={[route]}>
-      <StoreProvider initialState={initialState}>
+      <JestStoreProvider initialState={initialState}>
         <I18nextProvider i18n={i18nForTests}>
           <Suspense fallback="">
             {component}
           </Suspense>
         </I18nextProvider>
-      </StoreProvider>
+      </JestStoreProvider>
     </MemoryRouter>,
   );
 }
