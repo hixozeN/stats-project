e0bd0d34b7593298365f078d3cc8e05093f5a17d
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2024-04-10 22:31:12 +0300

    добавляет модалку с соглашением

---
+++ src/widgets/Footer/ui/TermsOfUse/TermsOfUse.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable */
+import { Modal } from 'shared/ui/Modal/Modal';
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './TermsOfUse.module.scss';
+
+interface ITermsOfUse {
+  className?: string,
+  isOpen: boolean,
+  handleClose: () => void;
+}
+export const TermsOfUse = (props: ITermsOfUse) => {
+  const { className, isOpen, handleClose } = props;
+
+  return (
+    <Modal isOpen={isOpen} onClose={handleClose}>
+      <div className={classNames(cls.TermsOfUse, {}, [className])}>
+        <h1 className={cls.title}>Условия использования сервиса статистики игроков игры Tanks Blitz:</h1>
+        <ol className={cls.list}>
+          <li className={cls.item}>
+            <span className={cls.bold}>Общее положение:</span>
+            Этот сервис статистики игроков игры Tanks Blitz предоставляется как инструмент
+            для отслеживания и анализа игровой статистики и не имеет связи или принадлежности
+            к ОБЩЕСТВО С ОГРАНИЧЕННОЙ ОТВЕТСТВЕННОСТЬЮ &quot;ЛЕСТА ГЕЙМС ЭДЖЕНСИ&quot;.
+          </li>
+          <li className={cls.item}>
+            <span className={cls.bold}>Права и ответственность пользователя: </span>
+            Настоящий сервис предоставляется исключительно для личного пользования.
+            Пользователь несет полную ответственность за любые действия, произведенные с использованием данного сервиса.
+          </li>
+          <li className={cls.item}>
+            <span className={cls.bold}>Конфиденциальность: </span>
+            Сервис обязуется соблюдать конфиденциальность данных игроков
+            и не передавать их третьим лицам без согласия пользователя.
+          </li>
+          <li className={cls.item}>
+            <span className={cls.bold}>Отказ от ответственности: </span>
+            Сервис не несет ответственности за возможные ошибки в отображаемых данных,
+            а также за любые убытки или повреждения, возникшие в результате использования данного сервиса.
+          </li>
+          <li className={cls.item}>
+            <span className={cls.bold}>Изменения в условиях использования: </span>
+            Сервис оставляет за собой право вносить изменения
+            в условия использования без предварительного уведомления пользователей.
+          </li>
+          <li className={cls.item}>
+            <span className={cls.bold}>Контактная информация: </span>
+            Для связи с сервисом и разрешения возможных вопросов, пользователи могут обращаться по электронной почте:
+            {' '}
+            <a href="mailto:contact@royalarena.ru" className={cls.link}>contact@royalarena.ru</a>
+          </li>
+        </ol>
+      </div>
+    </Modal>
+  );
+};
