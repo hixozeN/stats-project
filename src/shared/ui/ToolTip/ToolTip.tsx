d159642743991d69a118a9dde099f96ac12d952d
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2024-05-08 05:56:42 +0300

    добавляет пропс className

--- src/shared/ui/ToolTip/ToolTip.tsx
+++ src/shared/ui/ToolTip/ToolTip.tsx
@@ -2,13 +2,14 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './ToolTip.module.scss';
 
 interface ToolTipProps<T> {
+  className?: string;
   text: T;
   isVisible: boolean;
 }
 
-function ToolTip<T>({ text, isVisible }: ToolTipProps<T>) {
+function ToolTip<T>({ className, text, isVisible }: ToolTipProps<T>) {
   return (
-    <span className={classNames(cls.tooltip, { [cls.visible]: isVisible }, [])}>
+    <span className={classNames(cls.tooltip, { [cls.visible]: isVisible }, [className])}>
       <span className={classNames(cls.cloud)}>{text}</span>
     </span>
   );
