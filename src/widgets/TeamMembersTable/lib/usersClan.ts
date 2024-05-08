6a1065921ee54080d724fdcaa76412b6160d7738
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2024-05-08 05:55:37 +0300

    корректирует функцию сортировки юзеров

--- src/widgets/TeamMembersTable/lib/usersClan.ts
+++ src/widgets/TeamMembersTable/lib/usersClan.ts
@@ -1,39 +1,33 @@
-import { LestaClanMember, LestaClanPlayer, LestaClanUser } from 'entities/Lesta/model/types/clans';
+import { LestaClanMember } from 'entities/Lesta/model/types/clans';
 
-const roleValues: Record<string, string> = {
-  private: 'Рядовой',
-  executive_officer: 'Зам',
-  commander: 'Командир',
-};
+export function usersClan(clanUsers: LestaClanMember[]): LestaClanMember[] {
+  if (!clanUsers.length) return [];
 
-export function usersClan(players: LestaClanPlayer[], members: LestaClanMember[]): LestaClanUser[] {
-  if (!players.length || !members.length) return null;
+  const roleValues: Record<string, string> = {
+    private: 'private',
+    executive_officer: 'executive_officer',
+    commander: 'commander',
+  };
 
-  const resultArray: LestaClanUser[] = [];
-  const membersWithRoleMap: Record<number, LestaClanMember> = {};
-
-  members.forEach((member) => {
-    membersWithRoleMap[member.account_id] = {
-      ...member,
-      role: roleValues[member.role],
-    };
+  const usersWithRole = clanUsers.map((user) => {
+    if ((user as LestaClanMember).role) {
+      return {
+        ...user,
+        role: roleValues[(user as LestaClanMember).role],
+      };
+    }
+    return user;
   });
 
-  const concatenatedPlayers = players.map((player) => {
-    const matchingMember = membersWithRoleMap[player.account_id];
-    return matchingMember ? { ...player, ...matchingMember } : player;
-  });
+  const commanders = usersWithRole.filter((user) => user.role === roleValues.commander);
+  const executiveOfficers = usersWithRole.filter((user) => user.role === roleValues.executive_officer);
+  const otherUsers = usersWithRole.filter((user) => user.role !== roleValues.commander && user.role !== roleValues.executive_officer);
 
-  const sortedPlayers: LestaClanMember[] = concatenatedPlayers.sort((a, b) => (
-    a.nickname.localeCompare(b.nickname)
-  ));
+  const sortedUsers = [
+    ...commanders,
+    ...executiveOfficers,
+    ...otherUsers,
+  ];
 
-  const officers = sortedPlayers.filter((p) => p.role === roleValues.executive_officer);
-  const privates = sortedPlayers.filter((p) => p.role === roleValues.private);
-
-  resultArray.push(sortedPlayers.find((p) => p.role === roleValues.commander));
-  resultArray.push(...officers);
-  resultArray.push(...privates);
-
-  return resultArray;
+  return sortedUsers as LestaClanMember[];
 }
