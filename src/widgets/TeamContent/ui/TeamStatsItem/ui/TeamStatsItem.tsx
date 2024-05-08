7020c170c963f5af604e769842a1bb0a0073ecd1
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2024-05-08 06:37:22 +0300

    добавляет подсказку для wn8

--- src/widgets/TeamContent/ui/TeamStatsItem/ui/TeamStatsItem.tsx
+++ src/widgets/TeamContent/ui/TeamStatsItem/ui/TeamStatsItem.tsx
@@ -1,5 +1,7 @@
 import { ReactElement } from 'react';
 import { useTranslation } from 'react-i18next';
+import { ToolTip } from 'shared/ui/ToolTip/ToolTip';
+import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './TeamStatsItem.module.scss';
 
 interface TeamStatsItemProps {
@@ -12,11 +14,18 @@ export const TeamStatsItem = (props: TeamStatsItemProps) => {
   const { icon, title, result } = props;
   const { t } = useTranslation('teamPage');
 
+  const isVisible = title === 'WN8' && result === 0;
+
   return (
     <li className={cls.item}>
       <div className={cls.icon}>{icon}</div>
-      <span className={cls.text}>{t(`${title}`)}</span>
-      <span className={cls.results}>{result}</span>
+      <div className={cls.container}>
+        <span className={cls.text}>{t(`${title}`)}</span>
+        <span className={classNames(cls.results, { [cls.visible]: isVisible }, [])}>
+          {title === 'WN8' && result === 0 ? 'нет' : result}
+          <ToolTip text={t('TOOLTIP')} isVisible={isVisible} className={cls.tooltip} />
+        </span>
+      </div>
     </li>
   );
 };
