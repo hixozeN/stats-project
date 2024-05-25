3f9830836b56bb9e66d298179b05e0f4ad7e263d
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2024-05-26 02:07:06 +0300

    корректирует типы

--- src/features/Search/model/types/FoundData.ts
+++ src/features/Search/model/types/FoundData.ts
@@ -1,14 +1,16 @@
-export type FoundPlayerData = {
-  nickname: string,
-  account_id: number,
-}
-
 export type FoundClanData = {
   members_count: number;
   created_at: number;
   clan_id: number;
   tag: string;
   name: string;
+  emblem_set_id?: number;
+}
+
+export type FoundPlayerData = {
+  nickname: string;
+  account_id: number;
+  clan: FoundClanData;
 }
 
 export interface FoundData {
