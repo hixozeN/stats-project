5898f9407788bf43537a80da51199a668c0b6ce4
Vladislav K. <116466726+hixozeN@users.noreply.github.com>
2024-05-27 03:16:46 +0300

    Rename and move component on a lower level

---
+++ src/features/editCurrentUserPorfile/ui/AvailableUserAvatars/AvailableUserAvatarList.tsx
@@ -0,0 +1,29 @@
+import { memo } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { AvatarFullData } from 'entities/User';
+import { AvailableUserAvatar } from './AvailableUserAvatar';
+import cls from './AvailableUserAvatars.module.scss';
+
+interface AvailableUserAvatarsProps {
+  className?: string;
+  avatars: AvatarFullData[];
+  sectionName: string;
+  onCloseModal: () => void;
+}
+
+export const AvailableUserAvatarList = memo((props: AvailableUserAvatarsProps) => {
+  const {
+    className, avatars, sectionName, onCloseModal,
+  } = props;
+
+  return (
+    <section className={cls.avatars} onScroll={(e) => e.preventDefault()}>
+      <h2 className={cls.sectionHeading}>{sectionName}</h2>
+      <ul className={classNames(cls.avatarList, {}, [className])}>
+        {
+          avatars?.map((avatar) => <AvailableUserAvatar key={avatar._id} avatar={avatar} onCloseModal={onCloseModal} />)
+        }
+      </ul>
+    </section>
+  );
+});
