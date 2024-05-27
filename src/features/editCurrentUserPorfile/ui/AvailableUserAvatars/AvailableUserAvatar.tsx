2905eea911f2d36e9baca502501cc89e950887dd
Vladislav K. <116466726+hixozeN@users.noreply.github.com>
2024-05-27 03:17:41 +0300

    Add focus border for current user avatar

--- src/features/editCurrentUserPorfile/ui/AvailableUserAvatars/AvailableUserAvatar.tsx
+++ src/features/editCurrentUserPorfile/ui/AvailableUserAvatars/AvailableUserAvatar.tsx
@@ -1,9 +1,11 @@
-import { memo, useCallback } from 'react';
-import { AvatarFullData } from 'entities/User';
+import { memo, useCallback, useMemo } from 'react';
+import { AvatarFullData, getCurrentUserAvatar } from 'entities/User';
 import { useAppDispatch } from 'shared/hooks/useAppDispatch/useAppDispatch';
 import { useToasts } from 'shared/hooks/useToasts/useToasts';
 import { useTranslation } from 'react-i18next';
-import { SERVER_ERROR_MESSAGE } from 'shared/consts/global';
+import { DEFAULT_USER_AVATAR, SERVER_ERROR_MESSAGE } from 'shared/consts/global';
+import { useSelector } from 'react-redux';
+import { classNames } from 'shared/lib/classNames/classNames';
 import { patchCurrentUserAvatar } from '../../model/services/patchCurrentUserAvatar';
 import cls from './AvailableUserAvatars.module.scss';
 
@@ -17,6 +19,7 @@ export const AvailableUserAvatar = memo((props: AvailableUserAvatarProps) => {
   const { t } = useTranslation('profile');
   const dispatch = useAppDispatch();
   const { toastSuccess, toastWithError } = useToasts();
+  const currentAvatar = useSelector(getCurrentUserAvatar);
 
   const handleUpdateAvatar = useCallback((id: string) => {
     dispatch(patchCurrentUserAvatar({ avatarId: id }))
@@ -28,8 +31,16 @@ export const AvailableUserAvatar = memo((props: AvailableUserAvatarProps) => {
       .catch((e) => toastWithError(e?.response?.data?.message || SERVER_ERROR_MESSAGE));
   }, [dispatch, toastSuccess, toastWithError, t, onCloseModal]);
 
+  const avatarMod = useMemo(() => ({
+    [cls.active]: currentAvatar === avatar.image || currentAvatar === DEFAULT_USER_AVATAR,
+  }), [currentAvatar, avatar.image]);
+
   return (
-    <li className={cls.avatarItem} key={avatar._id} onClick={() => handleUpdateAvatar(avatar._id)}>
+    <li
+      className={classNames(cls.avatarItem, avatarMod)}
+      key={avatar._id}
+      onClick={() => handleUpdateAvatar(avatar._id)}
+    >
       <img className={cls.avatarImage} src={avatar.image} alt={avatar.name} />
       <span className={cls.avatarName}>{avatar.name}</span>
     </li>
