81b2335d377a5fae64b93f29b1a980790ac06b1a
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2024-10-21 20:39:59 +0300

    Добавляет константы на соц. сети

---
+++ src/shared/consts/socialLinks.ts
@@ -0,0 +1,6 @@
+export const LINKS = {
+  VK: 'https://vk.com/olrcyteam',
+  DISCORD: 'https://discord.gg/uewK7jKqZa',
+  TELEGRAM: 'https://t.me/blitzstats',
+  TELEGRAM_CHAT: 'https://t.me/blitzstatschat',
+};
