d3620274b1ca38c436885e218b007b7fa3b057b1
Aleksandra <mirvada1@yandex.ru>
2025-06-06 14:55:19 +0300

    добавляет пояснение к лоадеру и стилизует его

--- src/pages/AuthorizationLestaPage/AuthorizationLestaPage.tsx
+++ src/pages/AuthorizationLestaPage/AuthorizationLestaPage.tsx
@@ -1,11 +1,17 @@
 import Loader from 'shared/ui/Loader/Loader';
 import { useEffect } from 'react';
 import { openIDURL } from 'shared/consts/openID';
+import cls from './AuthorizationLestaPage.module.scss';
 
 export const AuthorizationLestaPage = () => {
   useEffect(() => {
     window.location.replace(openIDURL);
   }, []);
 
-  return <Loader />;
+  return (
+    <div className={cls.wrapper}>
+      <span className={cls.label}>{t('authPageLoaderLesta')}</span>
+      <Loader className={cls.loader} />
+    </div>
+  );
 };
