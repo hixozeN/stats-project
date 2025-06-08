361090c33d2bb91d3aa4f1860fea11448de8242f
Valeriya Izotova <my@valerka-made.ru>
2025-06-08 23:55:12 +0300

    исправляет ошибки по линтерам

--- src/widgets/UserProfile/ui/UserAvatar/UserAvatar.tsx
+++ src/widgets/UserProfile/ui/UserAvatar/UserAvatar.tsx
@@ -1,13 +1,13 @@
 import { memo } from 'react';
 import { useSelector } from 'react-redux';
-import { getUserAvatar, getUserDataLoadingStatus } from 'entities/Lesta/index';
+import { getUserAvatar, getUserDataLoadingStatus } from 'entities/Lesta';
 import defaultAvatar from 'shared/assets/images/default_avatar_resized.webp';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { Skeleton } from 'shared/ui/Skeleton/Skeleton';
 import { DEFAULT_USER_AVATAR } from 'shared/consts/global';
-import cls from './UserAvatar.module.scss';
 import { StatusBadge } from 'shared/ui/StatusBadge/StatusBadge';
+import cls from './UserAvatar.module.scss';
 
 interface UserAvatarProps {
   className?: string;
@@ -42,7 +42,7 @@ export const UserAvatar = memo((props: UserAvatarProps) => {
         src={getAvatarLink()}
         alt={t('Аватар пользователя')}
       />
-      <StatusBadge/>
+      <StatusBadge />
     </div>
   );
 });
