0e955ffaea8498b109d97c288b3fc686626c67a4
Aleksandra <mirvada1@yandex.ru>
2025-08-25 20:09:59 +0300

    добавляет инфо о  текущем пользователе

--- src/features/playersLeaderboard/ui/CurrentUserRating/CurrentUserRating.tsx
+++ src/features/playersLeaderboard/ui/CurrentUserRating/CurrentUserRating.tsx
@@ -1,6 +1,7 @@
 import { memo } from 'react';
 import { useSelector } from 'react-redux';
-import { getCurrentUserAccountId } from 'entities/User';
+import { getCurrentUserAccountId, getUserData } from 'entities/User';
+import { getLestaClanName, getLestaClanTag } from 'entities/Lesta';
 import { RatingItemSkeleton } from '../RatingItem/RatingItemSkeleton';
 import { RatingItem } from '../RatingItem/RatingItem';
 import {
@@ -17,8 +18,25 @@ export const CurrentUserRating = memo((props: CurrentUserRatingProps) => {
   const currentUserId = useSelector(getCurrentUserAccountId);
   const currentUserRatingData = useSelector(getCurrentUserRatingData);
   const isLoading = useSelector(getCurrentUserRatingLoadingStatus);
+  const user = useSelector(getUserData);
+  const clanTag = useSelector(getLestaClanTag);
+  const clanName = useSelector(getLestaClanName);
+  const userData = {
+    spa_id: currentUserId,
+    mmr: currentUserRatingData?.mmr ?? null,
+    season_number: currentUserRatingData?.season_number ?? null,
+    calibrationBattlesLeft: currentUserRatingData?.calibrationBattlesLeft ?? 10,
+    number: currentUserRatingData?.number ?? null,
+    percentile: currentUserRatingData?.percentile ?? null,
+    skip: currentUserRatingData?.skip ?? null,
+    updated_at: currentUserRatingData?.updated_at ?? null,
+    score: currentUserRatingData?.score ?? null,
+    nickname: user.lestaData.nickname,
+    clan_tag: clanTag,
+    clan_name: clanName,
+  };
 
-  if (!currentUserId || !currentUserRatingData) return null;
+  if (!currentUserId || !userData) return null;
 
   if (isLoading) {
     return (
@@ -29,7 +47,7 @@ export const CurrentUserRating = memo((props: CurrentUserRatingProps) => {
   return (
     <RatingItem
       isCurrentUser
-      player={currentUserRatingData}
+      player={userData}
       reward={
         currentUserRatingData?.number
           ? getRewardData(currentUserRatingData?.number)
